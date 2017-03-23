3c51a31e588667e08eda553b014df8585d3c4e53
Gopalakrishnan, Swami <s.gopalakrishnan@accenture.com>
2017-03-23 12:15:09 +0530

    changes for neural network

--- keras/keras_iris_neural_network.py
+++ keras/keras_iris_neural_network.py
@@ -11,14 +11,12 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
+from keras.optimizers import SGD
+
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
 
 # baseline_model
 def baseline_model():
@@ -26,6 +24,8 @@ def baseline_model():
     model = Sequential()
     model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
     model.add(Dense(3, init='normal', activation='softmax'))
+    #optimizer - adam or sgd
+    sgd = SGD(lr=0.3, decay=1e-6, momentum=0.9, nesterov=True)
     # compile
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
@@ -35,7 +35,7 @@ seed = 7
 numpy.random.seed(seed)
 
 # load the dataset
-dataframe = pandas.read_csv("iris.csv", delimiter="," , header=None)
+dataframe = pandas.read_csv("../datasets/iris.csv", delimiter="," , header=None)
 dataset = dataframe.values
 X = dataset[:,0:4].astype(float)
 y = dataset[:,4]
@@ -47,12 +47,12 @@ encoder_y = encoder.transform(y)
 # convert integers to dummy variables
 dummy_y = np_utils.to_categorical(encoder_y)
 
-# Split-out validation dataset
+ # Split-out validation dataset
 validation_size = 0.20
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, dummy_y, test_size=validation_size, random_state=seed)
 
 
-estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
+estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=500, batch_size=10, verbose=0)
 estimator.fit(X_train, Y_train)
 kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
 predictions = estimator.predict(X_validation)
